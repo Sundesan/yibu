5c43a945204e452d748fc0c6f9545a783153abbb
Eva <eva.anguita@rebelslab.com>
2024-12-06 15:28:29 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Log a message to the browser console
+console.log("JavaScript is working!");
+
+// Add an event listener to the button
+document.getElementById("testButton").addEventListener("click", function () {
+  alert("Button clicked!");
+});
